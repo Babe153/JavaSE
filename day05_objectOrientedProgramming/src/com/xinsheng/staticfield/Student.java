aff43722747d6eece43da4f5783eaea99db3585c
Xinsheng Liu <xliu0809@uni.sydney.edu.au>
2025-10-05 03:07:51 +1100

    Day05

--- day05_objectOrientedProgramming/src/com/xinsheng/staticfield/Student.java
+++ day05_objectOrientedProgramming/src/com/xinsheng/staticfield/Student.java
@@ -1,4 +1,29 @@
 package com.xinsheng.staticfield;
+//Student.java
 
 public class Student {
+    //1.静态变量（类变量）
+    //有static修饰，属于类的变量，在计算机内存中只有一份，被类里的所有对象共享
+    static String name;
+
+    //2.实例变量（对象的变量）
+    //没有static修饰，属于每一个对象的变量
+    int age;
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void printName(){
+        System.out.println(Student.name);
+    }
+
+    public void printAge(){
+        System.out.println(this.age);
+    }
 }
--- day05_objectOrientedProgramming/src/com/xinsheng/staticfield/Test.java
+++ day05_objectOrientedProgramming/src/com/xinsheng/staticfield/Test.java
@@ -1,4 +1,26 @@
 package com.xinsheng.staticfield;
+//Test.java
 
 public class Test {
+    public static void main(String[] args) {
+        Student.name = "小明";
+        System.out.println(Student.name);
+
+
+        Student s1 = new Student();
+        s1.name = "小红"; //不推荐这样定义静态变量，仅作演示，请使用类名.静态变量名称进行访问和修改
+        //s1.printName();
+
+        Student s2 = new Student();
+        s2.name = "小黄";
+        s2.printName(); //结果为小黄
+        s1.printName(); //结果为小黄
+        //因为name为静态变量 类中只有一份 内存中也只有一份储存空间 所以修改的是一个东西
+
+        s1.age = 18;
+        s2.age = 20;
+        s1.printAge();
+        s2.printAge();
+        //age为对象的变量 所以每个对象内存中都有一个age内存 所以打印出来不一样
+    }
 }
